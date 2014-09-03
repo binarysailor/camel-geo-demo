395c07706cb33fa9280d03dcaf244f27a5a40d9b
unknown <GTASDEV@ltpl003689.bravurasolutions.local>
2014-09-04 00:09:51 +0100

    from stream in to file

--- src/main/java/szymanski/cameldemo/App.java
+++ src/main/java/szymanski/cameldemo/App.java
@@ -7,13 +7,14 @@ public class App {
 	public static void main(String[] args) throws Exception {
 
 		DefaultCamelContext context = new DefaultCamelContext();
+		CamelUtils.stopAfterWindowClosed(context);
 		context.addRoutes(new RouteBuilder() {
 			@Override
 			public void configure() throws Exception {
-				from("file:/c:/temp/camel-in").to("file:/c:/temp/camel-out");
+				from("file://c:/temp/camel-in").to("file://c:/temp/camel-out");
+				from("stream:in").setHeader("CamelFileName", simple("from-streamin-${date:now:HH-mm-ss}.txt")).to("file://c:/temp/camel-out");
 			}
 		});
 		context.start();
-		CamelUtils.stopAfterWindowClosed(context);
 	}
 }
